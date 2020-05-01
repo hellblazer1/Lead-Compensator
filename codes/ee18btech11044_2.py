f2f22a0f1b9c3e8b7c4ba4dcebe4a767cad6e9e2
hellblazer1 <61204537+hellblazer1@users.noreply.github.com>
2020-05-01 12:35:10 +0530

    Update ee18btech11044_2.py

--- codes/ee18btech11044_2.py
+++ codes/ee18btech11044_2.py
@@ -7,35 +7,35 @@ import subprocess
 import shlex
 #end
 
+s1 = signal.lti([-3.084], [0,-1,-15.54], [10])
+s2 = signal.lti([],[0,-1],[10])
 
-x = []
-y = []
-for i in range(1,50):
-    s1 = signal.lti([], [0,-1], [i])
-    w, mag, phase = signal.bode(s1)
-    sys = (mag,phase,w)
-    gm,pm,wg,wp = con.margin(sys)
-    x.append(i)
-    y.append(pm)
-    #print('Phase Margin:' ,pm)
-    #print('Gain cross over frequency:' ,wg) 
-#plt.figure()
-#plt.semilogx(w, mag)    # Bode magnitude plot
-#plt.figure()
-#plt.semilogx(w, phase)  # Bode phase plot
-#plt.show()
+w1, mag1, phase1 = signal.bode(s1)
+w2, mag2, phase2 = signal.bode(s2)
+
+sys1 = (mag1,phase1,w1)
+gm1,pm1,wg1,wp1 = con.margin(sys1)
+sys2 = (mag2,phase2,w2)
+gm2,pm2,wg2,wp2 = con.margin(sys2)
+
+#print('pm1:',pm1)
+#print('wg1:',wg1)
+#print('pm2:',pm2)
+#print('wg2:',wg2)
+
+
 plt.figure()
-plt.ylabel('Phase margin')
-plt.xlabel('K')
-plt.title('Phase margin vs K')
-plt.plot(x,y)
-
-
+plt.semilogx(w2, phase2,label = 'Original System')
+plt.plot([wg1,wg1],[-180,(-180+pm1)],'r--',lineWidth=1,label='final phase margin')
+plt.plot([wg2,wg2],[-180,(-180+pm2)],'g--',lineWidth=1,label = 'initial phase margin')
+plt.semilogx(w1,phase1,label = 'Compensated system')
+plt.plot([0.01,100],[-180,-180],'b--',lineWidth=1,label='-180deg line')
+plt.title('Bode phase plot')
+plt.legend()
+plt.grid()
 #if using termux
-plt.savefig('./figs/ee18btech11044_2.pdf')
-plt.savefig('./figs/ee18btech11044_2.eps')
-subprocess.run(shlex.split("termux-open ./figs/ee18btech11044_2.pdf"))
-
-#else
+plt.savefig('./figs/ee18btech11044_2_1.pdf')
+plt.savefig('./figs/ee18btech11044_2_1.eps')
+subprocess.run(shlex.split("termux-open ./figs/ee18btech11044_2_1.pdf"))
 #plt.show()
 
